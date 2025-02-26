a6b2f7fc2f51bc1d44fc758fe85917deef686107
alumno <alumno@PC-V202-04.uacj.mx>
2025-02-26 10:13:56 -0700

    [agregar un componente apra la vista componente previsata]

---
+++ Agenda Shufle/Agenda Shufle/componentes/contacto_preview.swift
@@ -0,0 +1,23 @@
+//
+//  contacto_preview.swift
+//  Agenda Shufle
+//
+//  Created by alumno on 2/26/25.
+//
+
+import SwiftUI
+
+struct ContactoPrevista: View {
+    var body: some View {
+        HStack{
+            Image(systemName: "globe")
+            Text("hello you")
+            Text("hello you")
+            
+        }
+    }
+}
+
+#Preview {
+    ContactoPrevista()
+}
