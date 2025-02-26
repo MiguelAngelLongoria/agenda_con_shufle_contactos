13858ca17cbe340f7a6301edee7dd17b9e7d8365
alumno <alumno@PC-V202-04.uacj.mx>
2025-02-26 10:48:47 -0700

    cambios

---
+++ Agenda Shufle/Agenda Shufle/modelos/contacto_agenda.swift
@@ -0,0 +1,18 @@
+//
+//  contacto_agenda.swift
+//  Agenda Shufle
+//
+//  Created by alumno on 2/26/25.
+//
+
+import SwiftUI
+
+struct contacto_agenda: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    contacto_agenda()
+}
